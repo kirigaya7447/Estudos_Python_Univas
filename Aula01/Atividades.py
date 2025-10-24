b44508f65e13fd6d34975bae9c7aa91b39beca3a
kirigaya7447 <joaoh2915@gmail.com>
2025-10-23 21:59:16 -0300

    Arquivos primeira aula

---
+++ Aula01/Atividades.py
@@ -0,0 +1,28 @@
+mensagem = "Bem vindo à programação em Python"
+
+nome = input("Seu nome: \n")
+idade = input("Sua idade: \n")
+curso = input("Seu curso: \n")
+cidade = input("Sua cidade: \n")
+
+print("Olá, eu sou", nome, "tenho", idade, "anos, curso", curso, "e moro em", cidade)
+
+a = 1
+b = "1"
+c = 1.0
+d = True
+print(type(a),type(b),type(c),type(d))
+
+num1 = 432
+num2 = 87
+
+print("Soma:", num1 + num2, "\nSubtração:", num1 - num2, "\nMultiplicação:", num1 * num2, "\nSubtração:", num1 / num2)
+
+letra = input("Digite um número: ")
+numero = int(letra)
+soma = numero + 5
+
+estudando = True
+comFome = False
+print("Com fome:", comFome, " e estudando ", estudando)
+
---
+++ Aula01/Codigo.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+nome = input("Digite seu nome:\n")
+idade = input("Digite seu nome:\n")
+altura = 1.69
+trabalha = True
+print("Olá", nome)
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(trabalha))
+
+newIdade = int(idade)
+print(type(idade))
