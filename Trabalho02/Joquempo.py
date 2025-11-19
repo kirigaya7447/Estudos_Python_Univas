695b93f0f7cbc3ac8643bd7ff8b003c5d48f22f2
kirigaya7447 <joaoh2915@gmail.com>
2025-11-18 21:33:52 -0300

    Jogo ajustado

--- Trabalho02/Joquempo.py
+++ Trabalho02/Joquempo.py
@@ -10,34 +10,42 @@ entrada = int(input("Escolha uma jogada\n(1-Tesoura)\n(2-Pedra)\n(3-Papel)\nOpç
 
 while(entrada != 0):
     alea = random.randint(1,3)
+
     if(alea == entrada):
         print("Empate!")
         pontosC += 1
         pontosJ += 1
+
     elif(alea == 1 and entrada == 2):
         print("Jogador", nome," ganhou!")
         print("Pedra ganha de tesoura")
         pontosJ += 1
+
     elif(alea == 1 and entrada == 3):
         print("Computador ganhou!")
         print("Papel perde de tesoura")
         pontosC += 1
+
     elif(alea == 2 and entrada == 1):
         print("Computador ganhou!")
         print("Tesoura perde de pedra")
         pontosC += 1
+
     elif(alea == 2 and entrada == 3):
         print("Jogador", nome," ganhou!")
         print("Papel ganha de pedra")
         pontosJ += 1
+
     elif(alea == 3 and entrada == 1):
         print("Jogador", nome," ganhou!")
         print("Tesoura ganha de papel")
         pontosJ += 1
+
     elif(alea == 3 and entrada == 2):
         print("Computador ganhou!")
         print("Pedra perde de papel")
         pontosC += 1
+        
     else:
         print("Jogada inválida!")
 
